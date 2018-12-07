5a50e3bb448447bda29456531cb9be14c5459b8b
Robert8D <robertbijl@8d-games.nl>
2018-12-07 17:15:07 +0100

    Day3 - Progress!

--- Advent2018/Advent/Day3.cpp
+++ Advent2018/Advent/Day3.cpp
@@ -2,9 +2,11 @@
 #include "Helper.h"
 
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <string>
 #include <regex>
+#include <map>
 
 using namespace std;
 
@@ -21,16 +23,36 @@ void Day3::Part1()
 	regex base_regex("#(\\d+)...(\\d+),(\\d+):.(\\d+)x(\\d+)");
 	smatch base_match;
 
+	map<int, map<int, int>> fabricClaims;
+
 	for (auto& line : data) {
 		cout << line << endl;
 		if (regex_match(line, base_match, base_regex))
 		{
-			auto& number = base_match[1]; // number
-			auto& x = base_match[2];
-			auto& y = base_match[3];
-			auto& widthX = base_match[4];
-			auto& widthY = base_match[5];
+			int number = stoi(base_match[1]); // number
+			int dataX = stoi(base_match[2]);
+			int dataY = stoi(base_match[3]);
+			int widthX = stoi(base_match[4]);
+			int widthY = stoi(base_match[5]);
 
+			for (int x = dataX; x < dataX + widthX; x++) {
+				auto it = fabricClaims.find(x);
+
+				if (it == fabricClaims.end())
+					fabricClaims.insert(make_pair(x, map<int, int>()));
+
+				map<int, int> row = fabricClaims.at(x);
+				for (int y = dataY; y < dataY + widthY; y++) {
+					auto rowIt = row.find(y);
+					if (rowIt == row.end()) {
+						row.insert(make_pair(y, 1));
+					}
+					else {
+						rowIt->second += 1;
+					}
+
+				}
+			}
 		}
 		else
 			cout << "oops" << endl;
