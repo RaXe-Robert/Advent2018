c8176a149636e4e665fd0aa4d34298ec3dcdbe5e
Robert <robertbijl@live.nl>
2018-12-09 20:02:20 +0100

    Solved Day 4 - Part 1

--- Advent2018/Advent/Day4.cpp
+++ Advent2018/Advent/Day4.cpp
@@ -1,5 +1,6 @@
 #include "Day4.h"
 #include "FileHelper.h"
+#include "DateTime.h"
 
 #include <iostream>
 #include <algorithm>
@@ -7,6 +8,7 @@
 #include <string>
 #include <regex>
 #include <map>
+#include <chrono>
 
 using namespace std;
 
@@ -16,100 +18,96 @@ https://adventofcode.com/2018/day/4
 
 void Day4::Part1()
 {
-	cout << "Day3 part 1" << endl;
+	cout << "Day4 part 1" << endl;
 
-	vector<string> data = FileHelper::ReadFileToStringVector("D:\\input3.txt");
+	vector<string> data = FileHelper::ReadFileToStringVector("D:\\input4.txt");
 
-	regex base_regex("#(\\d+)...(\\d+),(\\d+):.(\\d+)x(\\d+)");
+	regex dateTime_regex("\\[(\\d+)\\-(\\d+)\\-(\\d+) (\\d+)\\:(\\d+)\\] (.*)");
+	regex guard_regex("(\\w+) \\#(\\d+)");
+
+	vector<GuardAction> guardActions;
+
 	smatch base_match;
-
-	map<int, map<int, int>> fabricClaims;
 	for (auto& line : data) {
-		if (regex_match(line, base_match, base_regex))
-		{
-			int dataX = stoi(base_match[2]);
-			int dataY = stoi(base_match[3]);
-			int maxX = dataX + stoi(base_match[4]);
-			int maxY = dataY + stoi(base_match[5]);
+		if (!regex_search(line, base_match, dateTime_regex))
+			continue;
 
-			for (int x = dataX; x < maxX; x++) {
-				auto it = fabricClaims.find(x);
+		int year = stoi(base_match[1]);
+		int month = stoi(base_match[2]);
+		int day = stoi(base_match[3]);
+		int hour = stoi(base_match[4]);
+		int minute = stoi(base_match[5]);
+		string action = base_match[6];
 
-				if (it == fabricClaims.end())
-					fabricClaims.insert(make_pair(x, map<int, int>()));
+		smatch guard_match;
+		if (regex_search(action, guard_match, guard_regex)) {
+			int guardId = stoi(guard_match[2]);
 
-				map<int, int>& row = fabricClaims.at(x);
-				for (int y = dataY; y < maxY; y++) {
+			guardActions.push_back(GuardAction(guardId, DateTime(year, month, day, hour, minute), "begins shift"));
+		}
+		else {
+			guardActions.push_back(GuardAction(-1, DateTime(year, month, day, hour, minute), action));
+		}
+	}
 
-					auto rowIt = row.find(y);
-					if (rowIt == row.end())
-						row.insert(make_pair(y, 1));
-					else
-						rowIt->second += 1;
-				}
+	sort(guardActions.begin(), guardActions.end(), [](GuardAction i, GuardAction j) {
+		return i.dateTime <= j.dateTime;
+	});
+
+	map<int, Guard> guards;
+	int currentGuard;
+
+	int startedSleeping;
+
+	for (auto& x : guardActions) {
+		if (x.guardId != -1) {
+			if (guards.find(x.guardId) == guards.end())
+				guards.insert(make_pair(x.guardId, Guard(x.guardId)));
+
+			currentGuard = x.guardId;
+		}
+		else {
+			if (x.action == "falls asleep") {
+				startedSleeping = x.dateTime.Minute();
+			}
+			else if (x.action == "wakes up") {
+				auto it = guards.find(currentGuard);
+				if (it != guards.end())
+					it->second.SetAsleepTime(startedSleeping, x.dateTime.Minute());
+				else
+					cout << "Failed - sorting probably went wrong" << endl;
 			}
 		}
 	}
 
-	int count = 0;
+	Guard longestSleeper;
+	int longestSleepTime = 0;
+	int longestMinute;
+	for (auto& guard : guards) {
+		int sum = 0;
+		int currLongestMinute = 0;
 
-	for (auto const& x : fabricClaims) {
-		for (auto const& y : x.second) {
-			if (y.second >= 2)
-				count++;
+		for (int i = 0; i < guard.second.asleep.size(); i++) {
+			sum += guard.second.asleep[i];
+			if (guard.second.asleep[i] > guard.second.asleep[currLongestMinute])
+				currLongestMinute = i;
+		}
+		if (sum > longestSleepTime) {
+			longestSleepTime = sum;
+			longestSleeper = guard.second;
+			longestMinute = currLongestMinute;
 		}
 	}
 
-	cout << "Answer: " << count << endl;
+	cout << "Answer: " << longestSleeper.guardId * longestMinute << ", Guard ID: " << longestSleeper.guardId << ", Longest minute: " << longestMinute << endl;
 }
 
 void Day4::Part2()
 {
-	cout << "Day3 part 2" << endl;
+	cout << "Day4 part 2" << endl;
 
-	vector<string> data = FileHelper::ReadFileToStringVector("D:\\input3.txt");
+	vector<string> data = FileHelper::ReadFileToStringVector("D:\\input4.txt");
 
 	regex base_regex("#(\\d+)...(\\d+),(\\d+):.(\\d+)x(\\d+)");
 	smatch base_match;
-
-	map<int, bool> numberHasOverlap;
-
-	map<int, map<int, vector<int>>> fabricClaims;
-	for (auto& line : data) {
-		if (regex_match(line, base_match, base_regex)) {
-			int number = stoi(base_match[1]);
-			int dataX = stoi(base_match[2]);
-			int dataY = stoi(base_match[3]);
-			int maxX = dataX + stoi(base_match[4]);
-			int maxY = dataY + stoi(base_match[5]);
-
-			for (int x = dataX; x < maxX; x++) {
-				if (fabricClaims.find(x) == fabricClaims.end())
-					fabricClaims.insert(make_pair(x, map<int, vector<int>>()));
-
-				map<int, vector<int>>& row = fabricClaims.at(x);
-				for (int y = dataY; y < maxY; y++) {
-					if (row.find(y) == row.end())
-						row.insert(make_pair(y, vector<int>()));
-
-					if (numberHasOverlap.find(number) == numberHasOverlap.end())
-						numberHasOverlap.insert(make_pair(number, false));
-
-					vector<int>& column = row.at(y);
-					column.push_back(number);
-
-					if (column.size() > 1) {
-						for (auto& value : column) {
-							numberHasOverlap.find(value)->second = true;
-						}
-					}
-				}
-			}
-		}
-	}
-
-	for (auto& number : numberHasOverlap) {
-		if (!number.second)
-			cout << "Answer: " << number.first << endl;
-	}
 }
