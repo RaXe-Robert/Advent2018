5e362f8803797c904a0353f40c6f1dd28815d8e2
Robert <robertbijl@live.nl>
2018-12-12 20:02:43 +0100

    Solved Day 5 - Part 2

--- Advent2018/Advent/Day5.cpp
+++ Advent2018/Advent/Day5.cpp
@@ -9,26 +9,60 @@ using namespace std;
 
 void Day5::Solve()
 {
-	string data = FileHelper::ReadFileToString("D:\\input5.txt");
+	string polymer = FileHelper::ReadFileToString("D:\\input5.txt");
+	vector<char> characters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
+	int result = SolvePolymer(polymer);
+	cout << "(Part 1) Answer: " << result << endl;
+
+	int smallestResult = result;
+	char bestCharacter;
+	for (auto& character : characters) {
+		string modifiedPolymer = ExcludeCharacter(polymer, character);
+		result = SolvePolymer(modifiedPolymer);
+
+		if (result <= smallestResult) {
+			smallestResult = result;
+			bestCharacter = character;
+		}
+
+		cout << "(Part2) " << character << ", result: " << result << endl;
+	}
+	cout << "(Part2) Answer: " << bestCharacter << ", with: " << smallestResult << endl;
+}
+
+int Day5::SolvePolymer(string polymer)
+{
 	locale loc;
 
 	bool noMoreOccurencess = false;
 	while (!noMoreOccurencess) {
 		bool stop = true;
-		for (int i = data.size() - 2; i >= 0; i--) {
+		for (int i = polymer.size() - 2; i >= 0; i--) {
 			// Check if same character
-			if (tolower(data[i], loc) != tolower(data[i + 1], loc))
+			if (tolower(polymer[i], loc) != tolower(polymer[i + 1], loc))
 				continue;
 
-			if ((isupper(data[i]) && islower(data[i + 1]))
-				|| (islower(data[i]) && isupper(data[i + 1]))) {
-				data.erase(i, 2);
+			if ((isupper(polymer[i]) && islower(polymer[i + 1]))
+				|| (islower(polymer[i]) && isupper(polymer[i + 1]))) {
+				polymer.erase(i, 2);
 				stop = false;
 			}
 		}
 		noMoreOccurencess = stop;
 	}
 
-	cout << "Answer: " << data.size() << endl;
+	return polymer.size();
+}
+
+string Day5::ExcludeCharacter(string polymer, char character)
+{
+	locale loc;
+
+	for (int i = polymer.size() - 1; i >= 0; i--) {
+		if (tolower(polymer[i], loc) == character)
+			polymer.erase(i, 1);
+	}
+
+	return polymer;
 }
--- Advent2018/Advent/Day5.h
+++ Advent2018/Advent/Day5.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <string>
+
 class Day5
 {
 public:
 
 	static void Solve();
+
+private:
+	static int SolvePolymer(std::string polymer);
+	static std::string ExcludeCharacter(std::string polymer, char character);
 };
