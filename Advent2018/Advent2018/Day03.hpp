56733095d03a116b0866bf01be9d5df46c87edbd
Robert Bijl <robertbijl@live.nl>
2019-11-20 14:51:07 +0100

    Fixed and cleaned up Day03

--- Advent2018/Advent2018/Day03.hpp
+++ Advent2018/Advent2018/Day03.hpp
@@ -12,42 +12,39 @@ void part1(char* data)
 	std::regex regex("#(\\d+)...(\\d+),(\\d+):.(\\d+)x(\\d+)");
 	std::cmatch match;
 
-	auto dataPtr = data;
-
 	std::map<int, std::map<int, int>> fabricClaims;
 	for (auto i = 0; i < DAY3_FILE_SIZE; i++) {
-		auto line = dataPtr + i * DAY3_BUFFER_SIZE;
+		auto line = data + i * DAY3_BUFFER_SIZE;
 		
-		if (std::regex_search(line, match, regex))
-		{
-			int dataX = stoi(match[2]);
-			int dataY = stoi(match[3]);
-			int maxX = dataX + stoi(match[4]);
-			int maxY = dataY + stoi(match[5]);
+		if (!std::regex_search(line, match, regex))
+			continue;
 
-			for (int x = dataX; x < maxX; x++) {
-				auto it = fabricClaims.find(x);
+		int dataX = stoi(match[2]);
+		int dataY = stoi(match[3]);
+		int maxX = dataX + stoi(match[4]);
+		int maxY = dataY + stoi(match[5]);
 
-				if (it == fabricClaims.end())
-					fabricClaims.insert(std::make_pair(x, std::map<int, int>()));
+		for (auto x = dataX; x < maxX; x++) {
+			auto it = fabricClaims.find(x);
 
-				std::map<int, int>& row = fabricClaims.at(x);
-				for (int y = dataY; y < maxY; y++) {
+			if (it == fabricClaims.end())
+				fabricClaims.insert(std::make_pair(x, std::map<int, int>()));
 
-					auto rowIt = row.find(y);
-					if (rowIt == row.end())
-						row.insert(std::make_pair(y, 1));
-					else
-						rowIt->second += 1;
-				}
+			std::map<int, int>& row = fabricClaims.at(x);
+			for (auto y = dataY; y < maxY; y++) {
+				auto rowIt = row.find(y);
+				if (rowIt == row.end())
+					row.insert(std::make_pair(y, 1));
+				else
+					rowIt->second += 1;
 			}
 		}
 	}
 	
 	int count = 0;
 
-	for (auto const& x : fabricClaims) {
-		for (auto const& y : x.second) {
+	for (auto x : fabricClaims) {
+		for (auto y : x.second) {
 			if (y.second >= 2)
 				count++;
 		}
@@ -66,41 +63,49 @@ void part2(char* data)
 	std::map<int, bool> numberHasOverlap;
 
 	std::map<int, std::map<int, std::vector<int>>> fabricClaims;
-	for (auto i = 0; i < ) {
-		auto line = data[i];
-		if (std::regex_match(line, match, regex)) {
-			int number = stoi(match[1]);
-			int dataX = stoi(match[2]);
-			int dataY = stoi(match[3]);
-			int maxX = dataX + stoi(match[4]);
-			int maxY = dataY + stoi(match[5]);
+	for (auto i = 0; i < DAY3_FILE_SIZE; i++) {
+		auto line = data + i * DAY3_BUFFER_SIZE;
 
-			for (int x = dataX; x < maxX; x++) {
-				if (fabricClaims.find(x) == fabricClaims.end())
-					fabricClaims.insert(std::make_pair(x, std::map<int, std::vector<int>>()));
+		if (!std::regex_search(line, match, regex))
+			continue;
 
-				std::map<int, std::vector<int>>& row = fabricClaims.at(x);
-				for (int y = dataY; y < maxY; y++) {
-					if (row.find(y) == row.end())
-						row.insert(std::make_pair(y, std::vector<int>()));
+		int number = stoi(match[1]);
+		int dataX = stoi(match[2]);
+		int dataY = stoi(match[3]);
+		int maxX = dataX + stoi(match[4]);
+		int maxY = dataY + stoi(match[5]);
+
+		for (int x = dataX; x < maxX; x++) {
+			if (fabricClaims.find(x) == fabricClaims.end()) {
+				auto pair = std::make_pair(x, std::map<int, std::vector<int>>());
+				fabricClaims.insert(pair);
+			}
+
+			std::map<int, std::vector<int>>& row = fabricClaims.at(x);
+			for (int y = dataY; y < maxY; y++) {
+				if (row.find(y) == row.end()) {
+					auto pair = std::make_pair(y, std::vector<int>());
+					row.insert(pair);
+				}
 	
-					if (numberHasOverlap.find(number) == numberHasOverlap.end())
-						numberHasOverlap.insert(std::make_pair(number, false));
+				if (numberHasOverlap.find(number) == numberHasOverlap.end()) {
+					auto pair = std::make_pair(number, false);
+					numberHasOverlap.insert(pair);
+				}
 
-					std::vector<int>& column = row.at(y);
-					column.push_back(number);
+				std::vector<int>& column = row.at(y);
+				column.push_back(number);
 
-					if (column.size() > 1) {
-						for (auto& value : column) {
-							numberHasOverlap.find(value)->second = true;
-						}
+				if (column.size() > 1) {
+					for (auto& value : column) {
+						numberHasOverlap.find(value)->second = true;
 					}
 				}
 			}
 		}
 	}
 
-	for (auto& number : numberHasOverlap) {
+	for (auto number : numberHasOverlap) {
 		if (!number.second) {
 			printf("[Day03][2] Answer: %i\n\n", number.first);
 			return;
@@ -113,10 +118,6 @@ void part2(char* data)
 void day03(const char* filepath)
 {
 	auto file = fopen(filepath, "rb");
-	int32_t size;
-	fseek(file, 0, SEEK_END);
-	size = ftell(file);
-	fseek(file, 0, SEEK_SET);
 
 	auto buffer = reinterpret_cast<char*>(malloc(1323 * 30));
 	auto start = buffer;
@@ -124,10 +125,6 @@ void day03(const char* filepath)
 	while (fgets(buffer, DAY2_BUFFER_SIZE, file))
 		buffer += 30;
 	fclose(file);
-	
-	//auto data = buffer;
-
-	//std::vector<char*> data = ReadFileToStringVector(filepath);
 
 	part1(start);
 	part2(start);
