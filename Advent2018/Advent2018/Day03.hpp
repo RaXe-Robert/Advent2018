a79f284b37b16efa733572e72d61524ebfe098bd
Robert Bijl <robertbijl@live.nl>
2019-11-22 10:58:42 +0100

    Day03 is now ten times faster

--- Advent2018/Advent2018/Day03.hpp
+++ Advent2018/Advent2018/Day03.hpp
@@ -6,11 +6,11 @@ https://adventofcode.com/2018/day/3
 #define DAY3_BUFFER_SIZE 30
 
 struct claim {
-	int id;
-	int x_start;
-	int x_end;
-	int y_start;
-	int y_end;
+	s16 id;
+	s16 x_start;
+	s16 x_end;
+	s16 y_start;
+	s16 y_end;
 };
 
 void part1(claim claims[])
@@ -20,36 +20,21 @@ void part1(claim claims[])
 	std::regex regex("#(\\d+)...(\\d+),(\\d+):.(\\d+)x(\\d+)");
 	std::cmatch match;
 
-	std::map<int, std::map<int, int>> claimsMap;
+	s16* claimGrid = new s16[1000 * 1000];
+	memset(claimGrid, 0, sizeof(s16) * 1000 * 1000);
+
+	int count = 0;
+
 	for (auto i = 0; i < DAY3_FILE_SIZE; i++) {
 		auto claim = claims[i];
 
 		for (auto x = claim.x_start; x < claim.x_end; x++) {
-			auto it = claimsMap.find(x);
-			if (it == claimsMap.end()) {
-				auto pair = std::make_pair(x, std::map<int, int>());
-				claimsMap.insert(pair);
-			}
-
-			auto& row = claimsMap.at(x);
 			for (auto y = claim.y_start; y < claim.y_end; y++) {
-				auto rowIt = row.find(y);
-				if (rowIt == row.end()) {
-					auto pair = std::make_pair(y, 1);
-					row.insert(pair);
-				}
-				else
-					(*rowIt).second += 1;
+				auto tile = claimGrid + (x * 1000 + y);
+				*tile += 1;
+				if (*tile == 2)
+					count++;		
 			}
-		}
-	}
-	
-	int count = 0;
-
-	for (auto x : claimsMap) {
-		for (auto y : x.second) {
-			if (y.second >= 2)
-				count++;
 		}
 	}
 
@@ -64,48 +49,29 @@ void part2(claim claims[])
 	std::cmatch match;
 
 	std::set<int> noOverlap;
-	// int's are respectively: x, y, claimId
-	std::map<int, std::map<int, int>> claimsMap;
+
+	s16* claimGrid = new s16[1000 * 1000];
+	memset(claimGrid, -1, sizeof(s16) * 1000 * 1000);
 
 	for (auto i = 0; i < DAY3_FILE_SIZE; i++) {
 		auto claim = claims[i];
 		noOverlap.insert(claim.id);
 
 		for (auto x = claim.x_start; x < claim.x_end; x++) {
-			if (claimsMap.find(x) == claimsMap.end()) {
-				auto pair = std::make_pair(x, std::map<int, int>());
-				claimsMap.insert(pair);
-			}
-
-			auto& col = claimsMap.at(x);
 			for (auto y = claim.y_start; y < claim.y_end; y++) {
-				auto row = col.find(y);
-				if (row == col.end()) {
-					auto claimPair = std::make_pair(y, claim.id);
-					col.insert(claimPair);
-				}
-				else {
-					auto existingClaimId = row->second;
-					if (existingClaimId == claim.id)
-						continue;
-					
-					if (existingClaimId != -1) {
-						noOverlap.erase(existingClaimId);
-						row->second = -1;
-					}
+				auto existingClaimId = claimGrid[x * 1000 + y];
 
+				if (existingClaimId == -1)
+					claimGrid[x * 1000 + y] = claim.id;
+				else {
+					noOverlap.erase(existingClaimId);
 					noOverlap.erase(claim.id);
 				}
 			}
 		}
 	}
 
-	for (auto number : noOverlap) {
-		printf("[Day03][2] Answer: %i\n\n", number);
-		return;
-	}
-
-	printf("[Day03][2] Answer: ERROR\n\n");
+	printf("[Day03][2] Answer: %i\n\n", *noOverlap.begin());
 }
 
 void day03(const char* filepath)
