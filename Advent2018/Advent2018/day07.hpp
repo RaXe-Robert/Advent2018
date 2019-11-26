f983bb263bb442da4c4ec236fbfca677faee8848
Robert Bijl <robertbijl@live.nl>
2019-11-26 13:14:36 +0100

    First steps for solving day07

--- Advent2018/Advent2018/day07.hpp
+++ Advent2018/Advent2018/day07.hpp
@@ -2,19 +2,43 @@
 https://adventofcode.com/2018/day/7
 */
 
+constexpr s32 ALPHABET_LENGTH = 26;
+constexpr s32 ASCII_OFFSET = 65;
+
 void day07(const char* filepath)
 {
+	s32* steps;
+	steps = reinterpret_cast<s32*>(malloc(sizeof(s32) * ALPHABET_LENGTH * ALPHABET_LENGTH));
+	memset(steps, 0, sizeof(s32) * ALPHABET_LENGTH * ALPHABET_LENGTH);
+
 	char* buffer = new char[64];
 
 	auto file = fopen(filepath, "rb");
-	while (fgets(buffer, 64, file)) {
-		char step = buffer[5];
-		char stepRequired = buffer[36];
-		printf("%c - %c\n", step, stepRequired);
+	while (fgets(buffer, 64, file)) 
+	{
+		s32 step = buffer[5] - ASCII_OFFSET;
+		s32 stepRequired = buffer[36] - ASCII_OFFSET;
+
+		auto ptr = steps + (step * ALPHABET_LENGTH) + stepRequired;	
+		*ptr = 1;
 	}
 	fclose(file);
 
-	// Read: https://stackoverflow.com/questions/26695393/c-and-when-to-use-delete
+	for (auto x = 0; x < ALPHABET_LENGTH; x++)
+	{
+		char step = x + ASCII_OFFSET;
+		printf("%c, requires: ", step);
+		for (auto y = 0; y < ALPHABET_LENGTH; y++)
+		{
+			bool required = *(steps + (x * ALPHABET_LENGTH) + y) == 1;
+			if (required)
+				printf("%c ", y + ASCII_OFFSET);
+			else
+				printf("- ", 0);
+
+		}
+		printf("\n");
+	} 	
 
 	delete buffer;
 }
