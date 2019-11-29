11c759ced745344651c659afcf635e052a1e5cf3
Robert Bijl <robertbijl@live.nl>
2019-11-29 13:14:13 +0100

    Solved day07

--- Advent2018/Advent2018/day07.hpp
+++ Advent2018/Advent2018/day07.hpp
@@ -7,8 +7,8 @@ constexpr s32 ASCII_OFFSET = 65;
 
 struct Worker
 {
-	char task;
-	s32 duration;
+	char task = 0;
+	s32 duration = 0;
 };
 
 inline bool
@@ -22,34 +22,8 @@ containsChar(char* arr, s32 size, char c)
 	return false;
 }
 
-void day07(const char* filepath)
+void day07_part1(s32* steps)
 {
-	s32* steps;
-	steps = reinterpret_cast<s32*>(malloc(sizeof(s32) * ALPHABET_LENGTH * ALPHABET_LENGTH));
-	memset(steps, 0, sizeof(s32) * ALPHABET_LENGTH * ALPHABET_LENGTH);
-
-
-	s32* steps2;
-	steps2 = reinterpret_cast<s32*>(malloc(sizeof(s32) * ALPHABET_LENGTH * ALPHABET_LENGTH));
-	memset(steps2, 0, sizeof(s32) * ALPHABET_LENGTH * ALPHABET_LENGTH);
-
-	char* buffer = new char[64];
-
-	auto file = fopen(filepath, "rb");
-	while (fgets(buffer, 64, file)) 
-	{
-		s32 stepRequired = buffer[5] - ASCII_OFFSET;
-		s32 step = buffer[36] - ASCII_OFFSET;
-
-		auto ptr = &steps[step * ALPHABET_LENGTH + stepRequired];	
-		*ptr = 1;
-
-		auto ptr2 = &steps2[step * ALPHABET_LENGTH + stepRequired];
-		*ptr2 = 1;
-	}
-	fclose(file);
-
-
 	char* part1 = new char[ALPHABET_LENGTH + 1];
 	memset(part1, 0, sizeof(char) * ALPHABET_LENGTH);
 	part1[ALPHABET_LENGTH] = '\0';
@@ -66,7 +40,7 @@ void day07(const char* filepath)
 
 		for (auto y = 0; y < ALPHABET_LENGTH; y++)
 		{
-			bool required = steps[(x * ALPHABET_LENGTH) + y] == 1;
+			bool required = steps[x * ALPHABET_LENGTH + y] == 1;
 			if (required)
 			{
 				available = false;
@@ -87,93 +61,117 @@ void day07(const char* filepath)
 	}
 
 	printf("[Day07][1] Answer: %s\n", part1);
+}
 
+void day07_part2(s32* steps)
+{
 	char* busy = new char[ALPHABET_LENGTH + 1];
 	memset(busy, 0, sizeof(char) * ALPHABET_LENGTH);
 	busy[ALPHABET_LENGTH] = '\0';
-
 	s32 busy_length = 0;
+
+	char* done = new char[ALPHABET_LENGTH + 1];
+	memset(done, 0, sizeof(char) * ALPHABET_LENGTH);
+	done[ALPHABET_LENGTH] = '\0';
+	s32 done_length = 0;
 
 	s32 part2 = 0;
 
-	const auto workerCount = 4;
+	const auto workerCount = 5;
 	auto workers = new Worker[workerCount];
 
-	while(1)
+	for (; part2 < (1 << 16); part2++)
 	{
+		for (auto i = 0; i < workerCount; i++)
+		{
+			auto worker = &workers[i];
+			if (worker->task == 0)
+				continue;
+
+			worker->duration--;
+			if (worker->duration > 0)
+				continue;
+
+			for (auto ii = 0; ii < ALPHABET_LENGTH; ii++)
+				steps[ii * ALPHABET_LENGTH + (worker->task - ASCII_OFFSET)] = 0;
+
+			done[done_length] = worker->task;
+			done_length++;
+
+			worker->duration = 0;
+			worker->task = 0;
+		}
+
 		for (char x = 0; x < ALPHABET_LENGTH; x++)
 		{
 			char c = x + ASCII_OFFSET;
-			if (containsChar(busy, ALPHABET_LENGTH, c))
+			if (containsChar(busy, busy_length, c))
 				continue;
 
 			bool available = true;
 
 			for (auto y = 0; y < ALPHABET_LENGTH; y++)
 			{
-				bool required = steps2[(x * ALPHABET_LENGTH) + y] == 1;
+				bool required = steps[x * ALPHABET_LENGTH + y] == 1;
 				if (required)
-				{
 					available = false;
-					break;
-				}
 			}
 
 			if (available)
 			{
-				auto stepTime = 60 + x;
+				auto stepTime = 60 + (x + 1);
 
 				for (auto i = 0; i < workerCount; i++)
 				{
-					if (workers[i].task == 0)
+					auto worker = &workers[i];
+					if (worker->task == 0)
 					{
-						workers[i].task = c;
-						workers[i].duration = stepTime;
-						
+						worker->task = c;
+						worker->duration = stepTime;
+
 						busy[busy_length] = c;
 						busy_length++;
 						break;
 					}
 				}
-
-				for (auto i = 0; i < ALPHABET_LENGTH; i++)
-					steps2[i * ALPHABET_LENGTH + x] = 0;
 			}
 		}
 
-		s32 shortestStep = -1;
-		for (auto i = 0; i < workerCount; i++)
-		{
-			// Idle
-			if (workers[i].duration == 0)
-				continue;
-
-			if (shortestStep == -1 || workers[i].duration < shortestStep)
-				shortestStep = workers[i].duration;
-		}
-
-		if (shortestStep > 0)
-			part2 += shortestStep;
-
-		for (auto i = 0; i < workerCount; i++)
-		{
-			workers[i].duration -= shortestStep;
-			if (workers[i].duration <= 0)
-			{
-				for (auto ii = 0; ii < ALPHABET_LENGTH; ii++)
-					steps2[ii * ALPHABET_LENGTH + (workers[i].task - ASCII_OFFSET)] = 0;
-
-				workers[i].task = 0;
-			}
-		}
-
-
-		// Safe guard
-		if (part2 >= 1 << 31)
+		if (done_length >= ALPHABET_LENGTH)
 			break;
 	}
 
-	printf("[Day07][2] Answer: %s\n", part2);
+	printf("[Day07][2] Answer: %i\n", part2);
+}
 
-	free(buffer);
+void day07(const char* filepath)
+{
+	s32* steps;
+	steps = reinterpret_cast<s32*>(malloc(sizeof(s32) * ALPHABET_LENGTH * ALPHABET_LENGTH));
+	memset(steps, 0, sizeof(s32) * ALPHABET_LENGTH * ALPHABET_LENGTH);
+
+	char* buffer = new char[64];
+	auto file = fopen(filepath, "rb");
+
+	while (fgets(buffer, 64, file)) 
+	{
+		s32 stepRequired = buffer[5] - ASCII_OFFSET;
+		s32 step = buffer[36] - ASCII_OFFSET;
+
+		auto ptr = &steps[step * ALPHABET_LENGTH + stepRequired];	
+		*ptr = 1;
+	}
+
+	fclose(file);
+	delete buffer;
+
+	s32* steps2;
+	steps2 = reinterpret_cast<s32*>(malloc(sizeof(s32) * ALPHABET_LENGTH * ALPHABET_LENGTH));
+	memcpy(steps2, steps, sizeof(s32) * ALPHABET_LENGTH * ALPHABET_LENGTH);
+
+	day07_part1(steps);
+	day07_part2(steps2);
+
+	free(steps);
+	free(steps2);
 }
