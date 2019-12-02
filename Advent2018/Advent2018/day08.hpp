7d35006c5dc1eb3c1b7db53bede93266a9206ca5
Robert Bijl <robertbijl@live.nl>
2019-12-02 16:15:41 +0100

    Solved day08 part1

--- Advent2018/Advent2018/day08.hpp
+++ Advent2018/Advent2018/day08.hpp
@@ -1,42 +1,71 @@
 /*
 https://adventofcode.com/2018/day/8
+
+2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2
+A----------------------------------
+	B----------- C-----------
+					 D-----
+
 */
+
 
 struct Node
 {
-	Node* children;
+	Node* children = 0;
 	s32 childCount;
 
-	s32* metadata;
+	s32* metadata = 0;
 	s32 metadataCount;
 
 	// Inclusive
 	s32 start;
-	// Exclusive
+	// Inclusive
 	s32 end;
-	
 };
 
 Node
-createNode(s32* arr[], s32 start, s32 end)
+createNode(s32* arr, s32 start, s32& part1)
 {
-	auto node = new Node;
-	node->start = start;
-	node->end = end;
+	Node node;
+	node.start = start;
 
-	node->childCount = *arr[start];
-	node->children = reinterpret_cast<Node*>(malloc(sizeof(Node) * node->childCount));
+	node.childCount = arr[start];
+	if (node.childCount > 0)
+		node.children = new Node[node.childCount];
 
-	node->metadataCount = *(*arr + start + 1);
-	node->metadata = reinterpret_cast<s32*>(malloc(sizeof(s32) * node->metadataCount));
-	memcpy(node->metadata, (*arr + (end - 1 - node->metadataCount)), node->metadataCount);
+	node.metadataCount = arr[start + 1];
+	if (node.metadataCount > 0)
+		node.metadata = new s32[node.metadataCount];
 
-	//for (auto i = node->start + 2; i < end; i++)
-	//{
-	//	Node* child = createNode(arr, start, end);
-	//}
+	node.end = (node.start + 1) + node.metadataCount;
 
-	return *node;
+	if (node.childCount > 0)
+	{
+		auto childStart = node.start + 2;
+		for (auto i = 0; i < node.childCount; i++)
+		{
+			auto child = createNode(arr, childStart, part1);
+			node.children[i] = child;
+			
+			// + 1 since end is inclusive
+			s32 childSize = (child.end - child.start) + 1;
+			node.end += childSize;
+
+			childStart += childSize;
+		}
+	}
+
+	if (node.metadataCount > 0)
+	{
+		auto start = node.end + 1 - node.metadataCount;
+		for (auto i = 0; i < node.metadataCount; i++)
+		{
+			node.metadata[i] = arr[start + i];
+			part1 += node.metadata[i];
+		}
+	}
+
+	return node;
 }
 
 void day08(const char* filepath)
@@ -60,7 +89,7 @@ void day08(const char* filepath)
 		s32 c = fgetc(file);
 		if (c == 32 || c == EOF)
 		{
-			s32 num = atoi(buffer);
+			s16 num = atoi(buffer);
 			arr[arr_length] = num;
 			arr_length++;
 
@@ -78,7 +107,10 @@ void day08(const char* filepath)
 		}
 	}
 
-	Node rootNode = createNode(&arr, 0, arr_length);
+	s32 part1 = 0;
+	Node rootNode = createNode(arr, 0, part1);
+
+	printf("[Day08][1] Answer: %i\n", part1);
 
 	fclose(file);
 
